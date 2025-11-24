4dd3934f680f9cc6f7d3b8a33ae134baed41c918
glyphor-adt <Kristina@Glyphorai.onmicrosoft.com>
2025-11-24 17:32:06 -0600

    Add src/components/Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="py-8 border-t border-gray-200">
+      <div className="container mx-auto text-center">
+        <p className="text-gray-500">Copyright © 2024 AI Chat Application. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
